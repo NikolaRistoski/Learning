7052fbb75484ab98a02db183bf0af610eb3ace00
NikolaRistoski <nikolaristoski@pabau.com>
2021-11-02 11:57:14 +0100

    Modules - Export and Import

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
-// Function that accepts number and return number
-function muliply(a: number, b: number) : number{
-    return a * b;
-}
+import * as multypleModule from  "./multiply" // Import All into single variable
+import { multyplyByTwo } from "./multiply"; // Import a single export from a module
+import { multyplyByTwo as byTwo } from "./multiply"; // Renamed import
+
 
 const a = 55;
 const b = 5;
 
-console.log(`${a} * ${b} = ${muliply(a, b)}`)
+console.log(multypleModule)
+
+console.log(`${a} * ${b} = ${multypleModule.default(a, b)}`)
+
+console.log(`Multiply by two from single variable inport: ${multypleModule.multyplyByTwo(10)}`)
+console.log(`Multiple by two from single export from module: ${multyplyByTwo(40)}`)
+console.log(`Multiple by two from renamed import: ${byTwo(40)}`)
---
+++ src/multiply.ts
@@ -0,0 +1,8 @@
+// Function that accepts number and return number
+export default function muliply(a: number, b:number) : number{
+    return a * b
+}
+
+export function multyplyByTwo(num: number){
+    return muliply(num, 2)
+}
