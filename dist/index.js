4c7181f3ed574221b4c67a14557975a7d646ede3
NikolaRistoski <nikolaristoski@pabau.com>
2021-11-02 11:31:55 +0100

    Setting Up a Simple TypeScript Project

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+// Function that accepts number and return number
+function muliply(a, b) {
+    return a * b;
+}
+const a = 55;
+const b = 5;
+console.log(`${a} * ${b} = ${muliply(a, b)}`);
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+// Function that accepts number and return number
+function muliply(a: number, b: number) : number{
+    return a * b;
+}
+
+const a = 55;
+const b = 5;
+
+console.log(`${a} * ${b} = ${muliply(a, b)}`)
